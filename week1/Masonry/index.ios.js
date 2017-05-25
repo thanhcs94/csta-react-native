6ad2e356cec731d5d798059f29b4e19c7edcb66d
thanhcs94 <nguyenvanthanh9294@gmail.com>
2017-05-25 23:11:51 +0700

    add-masonry-list

--- week1/Masonry/index.ios.js
+++ week1/Masonry/index.ios.js
@@ -4,50 +4,21 @@
  * @flow
  */
 
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
-
+import MasonryList from './src/components/masonrylist';
 export default class Masonry extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <MasonryList/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Masonry', () => Masonry);
---
+++ week1/Masonry/src/components/masonrylist.js
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Dimensions,
+} from 'react-native';
+const { width } = Dimensions.get('window');
+
+export default class MasonryList extends Component {
+  
+  numberOfColumns = 3;
+
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      dataSource: ds.cloneWithRows([]),
+    }
+  }
+
+  _getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  componentDidMount() {
+    let items = [];
+    for (let i = 0; i < 100; i++) {
+      items.push({
+        name: i.toString(),
+        color: this._getRandomColor(),
+        height: 10 + (Math.random() * 100),
+      });
+    }
+
+    let columns = [];
+    while(items.length > 0) {
+      const itemForEachColumn = items.splice(0, this.numberOfColumns);
+      for (let i = 0; i < itemForEachColumn.length; i++) {
+        if (columns[i] === undefined){
+          columns[i] = [];
+        }
+        columns[i].push(itemForEachColumn[i]);
+      }
+    }
+
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(columns),
+    })
+  }
+
+  _renderRow = (items, sectionId, rowIndex) => {
+    return (
+      <View style={{width: ((width - 4) / this.numberOfColumns) - 4, margin: 2, backgroundColor: 'red',}}>
+        { items.map((item, index) => {
+          const { name, color, height } = item;
+          return <Text key={index} style={{backgroundColor: color, height, marginVertical: 2,}}>name: {name}</Text>
+        })}
+      </View>
+    );
+  };
+
+  render() {
+    const { dataSource } = this.state;
+    return (
+        <ListView
+          enableEmptySections={true}
+          contentContainerStyle={{ flexDirection: 'row', backgroundColor: 'blue', width: width - 4, marginHorizontal: 2, }}
+          renderRow={this._renderRow}
+          dataSource={dataSource}
+          initialListSize={100}
+        />
+
+    );
+  }
+}
